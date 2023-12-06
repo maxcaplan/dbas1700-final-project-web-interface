e207af81124f483afd0d584f9eb4050d53b4357d
maxcaplan <maxacaplan@gmail.com>
2023-12-06 15:23:46 -0400

    Implemented updateStudent API endpoint

--- server/routes/student.js
+++ server/routes/student.js
@@ -1,6 +1,10 @@
 import express from "express";
-import mssql from "mssql"
-import { validate_student_data } from "../util/student.js";
+import mssql from "mssql";
+import {
+  updateStudentTypes,
+  validateAddStudentData,
+  validateUpdateStudentData,
+} from "../util/student.js";
 
 const router = express.Router();
 
@@ -11,12 +15,12 @@ const router = express.Router();
 // Make sure there is an active databse connection
 router.use((req, res, next) => {
   if (!req.app.locals.db || !req.app.locals.db.connected) {
-    res.status(500)
-    throw new Error("DATABASE CONNECTION ERROR")
+    res.status(500);
+    throw new Error("DATABASE CONNECTION ERROR");
   }
 
-  next()
-})
+  next();
+});
 
 //
 // ROUTES
@@ -24,66 +28,112 @@ router.use((req, res, next) => {
 
 // Get student by id
 router.get("/:id", (req, res) => {
-  res.send(req.params.id)
+  res.send(req.params.id);
 });
 
 // Add new student
 router.post("/addStudent", async (req, res) => {
   try {
-    console.log("Adding student...")
+    console.log("Adding student...");
 
     // Validate new student data
-    if (!validate_student_data(req.body)) {
+    if (!validateAddStudentData(req.body)) {
+      throw new Error("INVALID REQUEST DATA");
+    }
+
+    // Create new database request
+    const dbReq = new mssql.Request(req.app.locals.db);
+
+    // Set request input params
+    dbReq.input("FirstName", mssql.VarChar(50), req.body.FirstName);
+    dbReq.input("LastName", mssql.VarChar(50), req.body.LastName);
+    dbReq.input("DateOfBirth", mssql.Date, req.body.DateOfBirth);
+    dbReq.input("MajorID", mssql.Int, req.body.MajorID);
+
+    // Send insert query
+    const dbRes = await dbReq.query(
+      "INSERT INTO Student (FirstName, LastName, DateOfBirth, MajorID) VALUES(@FirstName, @LastName, @DateOfBirth, @MajorID)"
+    );
+
+    console.log(`Student added (${dbRes.rowsAffected} rows affected)`);
+    res.sendStatus(200);
+  } catch (e) {
+    throw e;
+  }
+});
+
+// Delete a student by id
+router.post("/deleteStudent", async (req, res) => {
+  try {
+    console.log("Deleting student...");
+
+    // Validate request StudentID
+    if (!Number.isInteger(req.body.StudentID))
+      throw new Error("INVALID REQUEST DATA");
+
+    // Create new database request
+    const dbReq = new mssql.Request(req.app.locals.db);
+
+    // Set request input params
+    dbReq.input("StudentID", mssql.Int, req.body.StudentID);
+
+    // Send delete query
+    const dbRes = await dbReq.query(
+      "DELETE FROM Student WHERE StudentID=@StudentID"
+    );
+
+    console.log(`Deleted student (${dbRes.rowsAffected} rows affected)`);
+    res.sendStatus(200);
+  } catch (e) {
+    throw e;
+  }
+});
+
+// Update a student by id
+router.post("/updateStudent", async(req, res) => {
+  try {
+    console.log("Updating student...")
+
+    // Validate update student data
+    if(!validateUpdateStudentData(req.body)) {
       throw new Error("INVALID REQUEST DATA")
     }
 
     // Create new database request
-    const dbReq = new mssql.Request(req.app.locals.db)
+    const dbReq = new mssql.Request(req.app.locals.db);
 
-    // Set request input params
-    dbReq.input("FirstName", mssql.VarChar(50), req.body.FirstName)
-    dbReq.input("LastName", mssql.VarChar(50), req.body.LastName)
-    dbReq.input("DateOfBirth", mssql.Date, req.body.DateOfBirth)
-    dbReq.input("MajorID", mssql.Int, req.body.MajorID)
+    // Deconstruct request body to only update fields
+    const { FirstName, LastName, DateOfBirth, MajorID } = req.body
+    const updateData = { FirstName, LastName, DateOfBirth, MajorID }
 
-    // Send insert query
-    const dbRes = await dbReq.query("INSERT INTO Student (FirstName, LastName, DateOfBirth, MajorID) VALUES(@FirstName, @LastName, @DateOfBirth, @MajorID)")
+    let values = []
     
-    console.log(`Student added (${dbRes.rowsAffected} rows affected)`)
-    res.sendStatus(200)
-  } catch(e) {
-    throw e
-  }
-})
+    // Loop over all update fields
+    for (const [key, value] of Object.entries(updateData)) {
+      // Skip key if value is not set
+      if(value === undefined) continue
 
-// Update a student by id
-router.post("/updateStudent", (req, res) => {
-  console.log("Updating student...")
-  res.send("Update student")
-})
+      // Add request input param
+      dbReq.input(key, updateStudentTypes[key], value)
 
-// Delete a student by id
-router.post("/deleteStudent", async(req, res) => {
-  try {
-    console.log("Deleting student...")
+      // Add update value to values arr
+      values.push(`${key} = @${key}`)
+    }
 
-    // Validate request StudentID
-    if(!Number.isInteger(req.body.StudentID)) throw new Error("INVALID REQUEST DATA")
-
-    // Create new database request
-    const dbReq = new mssql.Request(req.app.locals.db)
-
-    // Set request input params
+    // Set StudentID input param
     dbReq.input("StudentID", mssql.Int, req.body.StudentID)
 
-    // Send delete query
-    const dbRes = await dbReq.query("DELETE FROM Student WHERE StudentID=@StudentID")
+    // Construct query string from values array
+    const queryString = `UPDATE Student SET ${values.toString()} WHERE StudentID = @StudentID`
 
-    console.log(`Deleted student (${dbRes.rowsAffected} rows affected)`)
+    // Send update query
+    const dbRes = await dbReq.query(queryString)
+
+    console.log(`Student updated (${dbRes.rowsAffected} rows affected)`)
     res.sendStatus(200)
   } catch (e) {
-    throw e
+    throw e;
   }
-})
+});
 
-export { router }
+export { router };
--- server/util/student.js
+++ server/util/student.js
@@ -1,11 +1,62 @@
-export function validate_student_data(data) {
-    // Check that names and date of birth are all none empty strings
-    if(typeof data.FirstName !== 'string' || data.FirstName === "") return false
-    if(typeof data.LastName !== 'string' || data.LastName === "") return false
-    if(typeof data.DateOfBirth !== 'string' || data.DateOfBirth === "") return false
+import mssql from "mssql";
 
-    // If MajorID is not null, check that it is a valid int
-    if(data.MajorID && !Number.isInteger(data.MajorID)) return false
+/**
+ * Checks an add student request input data object. Returns true if input data is valid, else returns false
+ * @param {Object} data - Object containing input data for a new student
+ * @returns {Boolean}
+ */
+export function validateAddStudentData(data) {
+  // Check that names and date of birth are all none empty strings
+  if (typeof data.FirstName !== "string" || data.FirstName === "") return false;
+  if (typeof data.LastName !== "string" || data.LastName === "") return false;
+  if (typeof data.DateOfBirth !== "string" || data.DateOfBirth === "")
+    return false;
 
-    return true
+  // If MajorID is not null, check that it is a valid int
+  if (data.MajorID && !Number.isInteger(data.MajorID)) return false;
+
+  return true;
+}
+
+/**
+ * Checks an updated student request input data object. Returns true if input data is valid, else returns false
+ * @param {Object} data - Object containing input data for updating student
+ * @returns {Boolean}
+ */
+export function validateUpdateStudentData(data) {
+  // Check that an integer is set for StudentID
+  if (!data.StudentID || !Number.isInteger(data.StudentID)) return false;
+
+  // Check that FirstName is a string and not empty if defined
+  if (data.FirstName) {
+    if (typeof data.FirstName !== "string" || data.FirstName === "")
+      return false;
+  }
+
+  // Check that LastName is a string and not empty if defined
+  if (data.LastName) {
+    if (typeof data.LastName !== "string" || data.LastName === "") return false;
+  }
+
+  // Check that DateOfBirth is a string and not empty if defined
+  if (data.DateOfBirth) {
+    if (typeof data.DateOfBirth !== "string" || data.DateOfBirth === "")
+      return false;
+  }
+
+  // Check that MajorID is an int if defined
+  if (data.MajorID && !Number.isInteger(data.MajorID)) return false;
+
+  return true;
+}
+
+/**
+ * Mssql types for updateStudent data
+ */
+export const updateStudentTypes = {
+    StudentID: mssql.Int, 
+    FirstName: mssql.VarChar(50), 
+    LastName: mssql.VarChar(50), 
+    DateOfBirth: mssql.Date, 
+    MajorID: mssql.Int
 }
