0290315d35b077498cbde5471a79761d6ef5e454
maxcaplan <maxacaplan@gmail.com>
2023-12-06 14:33:14 -0400

    Implemented deleteStudent API endpoint

--- server/routes/student.js
+++ server/routes/student.js
@@ -34,22 +34,22 @@ router.post("/addStudent", async (req, res) => {
 
     // Validate new student data
     if (!validate_student_data(req.body)) {
-      throw new Error("INVALID INPUT DATA")
+      throw new Error("INVALID REQUEST DATA")
     }
 
     // Create new database request
     const dbReq = new mssql.Request(req.app.locals.db)
 
-    // Set input parameters
+    // Set request input params
     dbReq.input("FirstName", mssql.VarChar(50), req.body.FirstName)
     dbReq.input("LastName", mssql.VarChar(50), req.body.LastName)
     dbReq.input("DateOfBirth", mssql.Date, req.body.DateOfBirth)
     dbReq.input("MajorID", mssql.Int, req.body.MajorID)
 
     // Send insert query
-    await dbReq.query("INSERT INTO Student (FirstName, LastName, DateOfBirth, MajorID) VALUES(@FirstName, @LastName, @DateOfBirth, @MajorID)")
+    const dbRes = await dbReq.query("INSERT INTO Student (FirstName, LastName, DateOfBirth, MajorID) VALUES(@FirstName, @LastName, @DateOfBirth, @MajorID)")
     
-    console.log("Student added")
+    console.log(`Student added (${dbRes.rowsAffected} rows affected)`)
     res.sendStatus(200)
   } catch(e) {
     throw e
@@ -63,9 +63,27 @@ router.post("/updateStudent", (req, res) => {
 })
 
 // Delete a student by id
-router.post("/removeStudent", (req, res) => {
-  console.log("Removing student...")
-  res.send("Remove student")
+router.post("/deleteStudent", async(req, res) => {
+  try {
+    console.log("Deleting student...")
+
+    // Validate request StudentID
+    if(!Number.isInteger(req.body.StudentID)) throw new Error("INVALID REQUEST DATA")
+
+    // Create new database request
+    const dbReq = new mssql.Request(req.app.locals.db)
+
+    // Set request input params
+    dbReq.input("StudentID", mssql.Int, req.body.StudentID)
+
+    // Send delete query
+    const dbRes = await dbReq.query("DELETE FROM Student WHERE StudentID=@StudentID")
+
+    console.log(`Deleted student (${dbRes.rowsAffected} rows affected)`)
+    res.sendStatus(200)
+  } catch (e) {
+    throw e
+  }
 })
 
 export { router }
