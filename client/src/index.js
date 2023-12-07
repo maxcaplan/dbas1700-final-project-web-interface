ddec6acf4fd057b754297a26b2f567790f5a5590
maxcaplan <maxacaplan@gmail.com>
2023-12-07 11:55:05 -0400

    Added react router to client

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import reportWebVitals from "./reportWebVitals";
+
+// CSS
+import "./index.css";
+
+// Routes
+import Root from "./routes/root";
+
+// Create router
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+]);
+
+// Create react dom and start render
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
